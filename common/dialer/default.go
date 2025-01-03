67c6f378f037c5dc8f23b166276f1bfeeb21ef6f
lux5am <lux.5am@yandex.com>
2025-01-04 07:28:32 +0800

    add remotedst to clash restful api
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- common/dialer/default.go
+++ common/dialer/default.go
@@ -242,6 +242,9 @@ func (d *DefaultDialer) DialContext(ctx context.Context, network string, address
 	} else if address.IsFqdn() {
 		return nil, E.New("domain not resolved")
 	}
+	if metadata := adapter.ContextFrom(ctx); metadata != nil {
+		metadata.SetRemoteDst(address)
+	}
 	if d.networkStrategy == nil {
 		return d.trackConn(listener.ListenNetworkNamespace[net.Conn](d.netns, func() (net.Conn, error) {
 			switch N.NetworkName(network) {
@@ -312,6 +315,9 @@ func (d *DefaultDialer) DialParallelInterface(ctx context.Context, network strin
 }
 
 func (d *DefaultDialer) ListenPacket(ctx context.Context, destination M.Socksaddr) (net.PacketConn, error) {
+	if metadata := adapter.ContextFrom(ctx); metadata != nil {
+		metadata.SetRemoteDst(destination)
+	}
 	if d.networkStrategy == nil {
 		return d.trackPacketConn(listener.ListenNetworkNamespace[net.PacketConn](d.netns, func() (net.PacketConn, error) {
 			if destination.IsIPv6() {
