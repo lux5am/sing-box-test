13f092a480626a2c804de7e414a95a1f528fa078
lux5am <lux.5am@yandex.com>
2025-12-03 18:44:58 +0800

    use gobwas
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- experimental/clashapi/server.go
+++ experimental/clashapi/server.go
@@ -27,8 +27,8 @@ import (
 	"github.com/sagernet/sing/common/observable"
 	"github.com/sagernet/sing/service"
 	"github.com/sagernet/sing/service/filemanager"
-	"github.com/sagernet/ws"
-	"github.com/sagernet/ws/wsutil"
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
