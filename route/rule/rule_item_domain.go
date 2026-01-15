UptonEdward/sing-box

use origin fqdn when destination is fqdn

route/rule_item_domain.go
@@ -52,12 +52,12 @@ func NewRawDomainItem(matcher *domain.Matcher) *DomainItem {
 
 func (r *DomainItem) Match(metadata *adapter.InboundContext) bool {
 	var domainHost string
-	if metadata.SniffDomain != "" {
-		domainHost = metadata.SniffDomain
-	} else if metadata.Domain != "" {
-		domainHost = metadata.Domain
-	} else {
+	if metadata.Destination.IsFqdn() {
 		domainHost = metadata.Destination.Fqdn
+	} else if metadata.SniffDomain != "" {
+		domainHost = metadata.SniffDomain
+	} else {
+		domainHost = metadata.Domain
 	}
 	if domainHost == "" {
 		return false

route/rule_item_domain_keyword.go
@@ -18,12 +18,12 @@ func NewDomainKeywordItem(keywords []string) *DomainKeywordItem {
 
 func (r *DomainKeywordItem) Match(metadata *adapter.InboundContext) bool {
 	var domainHost string
-	if metadata.SniffDomain != "" {
-		domainHost = metadata.SniffDomain
-	} else if metadata.Domain != "" {
-		domainHost = metadata.Domain
-	} else {
+	if metadata.Destination.IsFqdn() {
 		domainHost = metadata.Destination.Fqdn
+	} else if metadata.SniffDomain != "" {
+		domainHost = metadata.SniffDomain
+	} else {
+		domainHost = metadata.Domain
 	}
 	if domainHost == "" {
 		return false

route/rule_item_domain_regex.go
@@ -39,12 +39,12 @@ func NewDomainRegexItem(expressions []string) (*DomainRegexItem, error) {
 
 func (r *DomainRegexItem) Match(metadata *adapter.InboundContext) bool {
 	var domainHost string
-	if metadata.SniffDomain != "" {
-		domainHost = metadata.SniffDomain
-	} else if metadata.Domain != "" {
-		domainHost = metadata.Domain
-	} else {
+	if metadata.Destination.IsFqdn() {
 		domainHost = metadata.Destination.Fqdn
+	} else if metadata.SniffDomain != "" {
+		domainHost = metadata.SniffDomain
+	} else {
+		domainHost = metadata.Domain
 	}
 	if domainHost == "" {
 		return false

lux5am/sing-box-test

Signed-off-by: lux5am <lux.5am@yandex.com>

route/rule/rule_item_domain.go
@@ -63,12 +63,12 @@ func NewRawDomainItem(matcher *domain.Matcher) *DomainItem {
 
 func (r *DomainItem) Match(metadata *adapter.InboundContext) bool {
 	var domainHost string
-	if metadata.SniffHost != "" {
-		domainHost = metadata.SniffHost
-	} else if metadata.Domain != "" {
-		domainHost = metadata.Domain
-	} else {
+	if metadata.Destination.IsFqdn() {
 		domainHost = metadata.Destination.Fqdn
+	} else if metadata.SniffHost != "" {
+		domainHost = metadata.SniffHost
+	} else {
+		domainHost = metadata.Domain
 	}
 	if domainHost == "" {
 		return false

route/rule/rule_item_domain_keyword.go
@@ -18,12 +18,12 @@ func NewDomainKeywordItem(keywords []string) *DomainKeywordItem {
 
 func (r *DomainKeywordItem) Match(metadata *adapter.InboundContext) bool {
 	var domainHost string
-	if metadata.SniffHost != "" {
-		domainHost = metadata.SniffHost
-	} else if metadata.Domain != "" {
-		domainHost = metadata.Domain
-	} else {
+	if metadata.Destination.IsFqdn() {
 		domainHost = metadata.Destination.Fqdn
+	} else if metadata.SniffHost != "" {
+		domainHost = metadata.SniffHost
+	} else {
+		domainHost = metadata.Domain
 	}
 	if domainHost == "" {
 		return false

route/rule/rule_item_domain_regex.go
@@ -39,12 +39,12 @@ func NewDomainRegexItem(expressions []string) (*DomainRegexItem, error) {
 
 func (r *DomainRegexItem) Match(metadata *adapter.InboundContext) bool {
 	var domainHost string
-	if metadata.SniffHost != "" {
-		domainHost = metadata.SniffHost
-	} else if metadata.Domain != "" {
-		domainHost = metadata.Domain
-	} else {
+	if metadata.Destination.IsFqdn() {
 		domainHost = metadata.Destination.Fqdn
+	} else if metadata.SniffHost != "" {
+		domainHost = metadata.SniffHost
+	} else {
+		domainHost = metadata.Domain
 	}
 	if domainHost == "" {
 		return false