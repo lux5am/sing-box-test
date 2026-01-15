wzming/sing-box

show sniff message in clash api

experimental/clashapi/trafficontrol/tracker.go
@@ -74,6 +74,7 @@ func (t TrackerMetadata) MarshalJSON() ([]byte, error) {
 			"sourcePort":      F.ToString(t.Metadata.Source.Port),
 			"destinationPort": F.ToString(t.Metadata.Destination.Port),
 			"host":            domain,
+			"sniffHosts":      t.Metadata.SniffHost,
 			"dnsMode":         "normal",
 			"processPath":     processPath,
 		},

lux5am/sing-box-test

Signed-off-by: lux5am <lux.5am@yandex.com>

experimental/clashapi/trafficontrol/tracker.go
@@ -74,6 +74,7 @@ func (t TrackerMetadata) MarshalJSON() ([]byte, error) {
 			"sourcePort":      F.ToString(t.Metadata.Source.Port),
 			"destinationPort": F.ToString(t.Metadata.Destination.Port),
 			"host":            domain,
+			"sniffHost":       t.Metadata.SniffHost,
 			"dnsMode":         "normal",
 			"processPath":     processPath,
 		},