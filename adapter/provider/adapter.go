FizxyNotsDev/sing-box

fix ommit provider subcription info if empty
Signed-off-by: lux5am <lux.5am@yandex.com>

adapter/provider/adapter.go
@@ -148,12 +148,15 @@ func getFirstLine(content string) (string, string) {
 }
 
 func (a *myProviderAdapter) SubInfo() map[string]int64 {
-	info := make(map[string]int64)
-	info["Upload"] = a.subInfo.upload
-	info["Download"] = a.subInfo.download
-	info["Total"] = a.subInfo.total
-	info["Expire"] = a.subInfo.expire
-	return info
+	if a.subInfo.upload != 0 || a.subInfo.download != 0 || a.subInfo.total != 0 || a.subInfo.expire != 0 {
+		info := make(map[string]int64)
+		info["Upload"] = a.subInfo.upload
+		info["Download"] = a.subInfo.download
+		info["Total"] = a.subInfo.total
+		info["Expire"] = a.subInfo.expire
+		return info
+	}
+	return nil
 }
 
 func parseSubInfo(infoString string) (SubInfo, bool) {

lux5am/sing-box-test

adapter/provider/adapter.go
@@ -148,12 +148,15 @@ func getFirstLine(content string) (string, string) {
 }
 
 func (p *myProviderAdapter) SubInfo() map[string]int64 {
-	info := make(map[string]int64)
-	info["Upload"] = p.subInfo.upload
-	info["Download"] = p.subInfo.download
-	info["Total"] = p.subInfo.total
-	info["Expire"] = p.subInfo.expire
-	return info
+	if p.subInfo.upload != 0 || p.subInfo.download != 0 || p.subInfo.total != 0 || p.subInfo.expire != 0 {
+		info := make(map[string]int64)
+		info["Upload"] = p.subInfo.upload
+		info["Download"] = p.subInfo.download
+		info["Total"] = p.subInfo.total
+		info["Expire"] = p.subInfo.expire
+		return info
+	}
+	return nil
 }
 
 func parseSubInfo(infoString string) (SubInfo, bool) {