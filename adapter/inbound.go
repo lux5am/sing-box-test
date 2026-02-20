lux5am/sing-box

add remotedst to clash restful api
Signed-off-by: lux5am <lux.5am@yandex.com>

adapter/inbound.go
@@ -51,6 +51,8 @@ type InboundContext struct {
 	User        string
 	Outbound    string
 
+	RemoteDst   *M.Socksaddr
+
 	// sniffer
 
 	Protocol     string
@@ -110,9 +112,32 @@ func (c *InboundContext) ResetRuleCache() {
 	c.DidMatch = false
 }
 
+func (c *InboundContext) InitRemoteDst() {
+	if c.RemoteDst == nil {
+		c.RemoteDst = new(M.Socksaddr)
+	}
+}
+
+func (c *InboundContext) SetRemoteDst(dst M.Socksaddr) {
+	if c.RemoteDst != nil {
+		c.RemoteDst.Addr = dst.Addr
+		c.RemoteDst.Port = dst.Port
+		c.RemoteDst.Fqdn = dst.Fqdn
+	}
+}
+
+func (c *InboundContext) GetRemoteDst() string {
+	if c.RemoteDst != nil {
+		return c.RemoteDst.AddrString()
+	} else {
+		return ""
+	}
+}
+
 type inboundContextKey struct{}
 
 func WithContext(ctx context.Context, inboundContext *InboundContext) context.Context {
+	inboundContext.InitRemoteDst()
 	return context.WithValue(ctx, (*inboundContextKey)(nil), inboundContext)
 }
 

lux5am/sing-box-test

adapter/inbound.go
@@ -107,6 +107,7 @@ type InboundContext struct {
 
 type InboundContextExtended struct {
 	RealOutbound string
+	RemoteDst    M.Socksaddr
 }
 
 func (c *InboundContext) InitExtended() {
@@ -124,6 +125,22 @@ func (c *InboundContext) SetRealOutbound(tag string) {
 func (c *InboundContext) GetRealOutbound() string {
 	if c.Extended != nil {
 		return c.Extended.RealOutbound
+	} else {
+		return ""
+	}
+}
+
+func (c *InboundContext) SetRemoteDst(dst M.Socksaddr) {
+	if c.Extended != nil {
+		c.Extended.RemoteDst.Addr = dst.Addr
+		c.Extended.RemoteDst.Port = dst.Port
+		c.Extended.RemoteDst.Fqdn = dst.Fqdn
+	}
+}
+
+func (c *InboundContext) GetRemoteDst() string {
+	if c.Extended != nil {
+		return c.Extended.RemoteDst.AddrString()
 	} else {
 		return ""
 	}