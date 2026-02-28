FizxyNotsDev/sing-box

fix provider reload
Signed-off-by: lux5am <lux.5am@yandex.com>

adapter/provider/adapter.go
@@ -82,6 +82,10 @@ func (a *myProviderAdapter) Path() string {
 	return a.path
 }
 
+func (a *myProviderAdapter) HealthcheckUrl() string {
+	return a.healthcheckUrl
+}
+
 func (a *myProviderAdapter) Type() string {
 	return a.providerType
 }
@@ -245,6 +249,11 @@ func decodeBase64Safe(content string) string {
 func (p *myProviderAdapter) checkChange(outbounds []option.Outbound) bool {
 	if len(p.lastOuts) != len(outbounds) {
 		return true
+	}
+	for i := range p.lastOuts {
+		if p.lastOuts[i].Tag != outbounds[i].Tag {
+			return true
+		}
 	}
 	outMap := make(map[string]option.Outbound, len(p.lastOuts))
 	for _, out := range p.lastOuts {

lux5am/sing-box-test

adapter/provider/adapter.go
@@ -82,6 +82,10 @@ func (p *myProviderAdapter) Path() string {
 	return p.path
 }
 
+func (p *myProviderAdapter) HealthcheckUrl() string {
+	return p.healthcheckUrl
+}
+
 func (p *myProviderAdapter) Type() string {
 	return p.providerType
 }
@@ -245,6 +249,11 @@ func decodeBase64Safe(content string) string {
 func (p *myProviderAdapter) checkChange(outbounds []option.Outbound) bool {
 	if len(p.lastOuts) != len(outbounds) {
 		return true
+	}
+	for i := range p.lastOuts {
+		if p.lastOuts[i].Tag != outbounds[i].Tag {
+			return true
+		}
 	}
 	outMap := make(map[string]option.Outbound, len(p.lastOuts))
 	for _, out := range p.lastOuts {